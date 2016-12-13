857824cebfecf9429f2390461c9b9e2871f2c8c0
eliezerwohl <eliezerwohl@gmail.com>
2016-12-12 19:50:50 -0500

    better solution

--- fizzbuzz/javascript.js
+++ fizzbuzz/javascript.js
@@ -20,3 +20,24 @@ for (var i = 1; i < 101; i++) {
 			console.log(i)
 		}
 }
+//better solution
+
+function fizzbuzz(fizz, buzz, number){
+	for (var i = 1; i <= number; i++) {
+	if ((i % buzz == 0) && (i % fizz == 0)){
+		console.log("fizzbuzz" + i)
+	}
+	else if (i % fizz == 0){
+		console.log("fizz")
+	}
+	else if(i % buzz == 0){
+		console.log("buzz")
+	}
+	else
+		{
+			console.log(i)
+		}
+	}
+}
+fizzbuzz(3,5, 100)
+
